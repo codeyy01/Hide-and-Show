ed02ded6e8780035f9afa4be3d0e585f4a5d481c
Mohammed Murshid <murshhi@gmail.com>
2025-06-12 15:09:35 +0530

    project 1.2

---
+++ index.js
@@ -0,0 +1,6 @@
+let showpass = document.getElementById("show_password");
+let password = document.getElementById("password");
+
+showpass.onclick = ()=>{
+    password.style.visibility = "visible";
+}
