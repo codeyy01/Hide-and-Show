77afdd9bd100c0575d27ca66b294b506400dab1c
Mohammed Murshid <murshhi@gmail.com>
2025-06-13 01:05:00 +0530

    Dark Theme

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ eyeIcon.onclick = ()=>{
         eyeIcon.src="images/eye-close.png";
     }
 }
+
+document.getElementById("icon").onclick = ()=>{
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src="images/sun.png";
+    }else{
+        icon.src = "images/moon.png";
+    }
+};
---
+++ uhu.py
@@ -0,0 +1,2 @@
+
+<i class="fa-regular fa-lightbulb"></i>
